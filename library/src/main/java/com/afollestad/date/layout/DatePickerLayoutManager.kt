d8c6533f9930d2424e37540c2bd20c9f4e6305c6
Aidan Follestad <drummer.aidan@gmail.com>
2019-07-03 12:57:00 -0700

    Formatting

--- library/src/main/java/com/afollestad/date/layout/DatePickerLayoutManager.kt
+++ library/src/main/java/com/afollestad/date/layout/DatePickerLayoutManager.kt
@@ -30,7 +30,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.CheckResult
-import androidx.core.widget.AutoSizeableTextView
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
